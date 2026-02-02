954d927e120ea3a196c29cabdd097b2742973f2c
Nikita Björknert <nb222tx@student.lnu.se>
2026-02-02 22:32:33 +0100

    Active fungerar

--- global.js
+++ global.js
@@ -1,25 +1,15 @@
-const listNav = {
-    list: "Uppgift: ",
-    assignment: ["Uppgift 1", "Uppgift 2", "Uppgift 3"]
-}; 
+const navList = {
+name: ["Startsida", "Uppgift 1", "Uppgift 2", "Uppgift 3"]
+};
 
-const globalNav = document.getElementById("globalNav");
+const globalNav = document.getElementById('globalNav');
 
-const assignment = listNav.assignment.join(", ");
-
-
-const current = localStorage.getItem("current");
-if (current) {
-    globalNav.value = current;
-}
-
-globalNav.addEventListener('click', function() {
-    localStorage.setItem("current", globalNav.value);
+document.querySelectorAll(".globalNav a.link").forEach(a => {
+    if (a.href === location.href) {
+        a.classList.add('active');
+    }
 });
-console.log("Byt sida");
 
-const allU = document.querySelectorAll("[li]");
-let currentU = "marked";
-if (localStorage.getItem('selectedU')) {
-    currentU = localStorage.getItem('selectedItem');
-}
+
+
+
