f4f967095cf5c6a2ad818b0e78b7f9f97468a4bf
Nikita Björknert <nb222tx@student.lnu.se>
2026-02-03 21:20:05 +0100

    3 feb

--- nav.js
+++ nav.js
@@ -1,14 +1,12 @@
-const navList = {
-name: ["Startsida", "Uppgift 1", "Uppgift 2", "Uppgift 3"]
-};
 
-const globalNav = document.getElementById('globalNav');
+const ass = document.querySelector('.globalNav');
 
 document.querySelectorAll(".globalNav a.link").forEach(a => {
     if (a.href === location.href) {
         a.classList.add('active');
     }
 });
+
 
 
 
---
+++ script.js
@@ -0,0 +1,52 @@
+const ass = document.querySelector('.globalNav');
+
+const assignments = [
+    {
+        id: '1a',
+        title: 'Hello',
+        description: 'Lära sig',
+        week: 1,
+        completed: true,
+        showLink: true,
+        difficulty: 'easy',
+        topics: ['Metoder', 'export', 'import']
+    },
+    {
+        id: '1b',
+        title: 'Hello2',
+        description: 'Lära sig att',
+        week: 1,
+        completed: false,
+        showLink: false,
+        difficulty: 'hard',
+        topics: ['git', 'versionshantering']
+    }
+];
+
+function createAssCard(ass) {
+    const card = document.createElement('div');
+    card.classList.add('ass-card');
+
+    card.innerHTML = `
+    <h3>Uppgift ${ass.id} - ${ass.title}</h3>
+    <p>${ass.description}</p>
+    <p>${ass.topics}</p>
+    <div class="ass-meta">
+        <span class="status ${ass.completed ? 'completed' : ''}"></span>
+        <span class="week">Vecka ${ass.week}</span>
+    </div>
+    ${ass.showLink ? `<a href="./${ass.id}/" class="ass-link">Öppna uppgift</a> 
+    ` : ''}
+    `;
+    return card;
+}
+function renderAss(assignments) {
+    const container = document.getElementById('container');
+
+    container.innerHTML = '';
+    for (const assignment of assignments) {
+        const card = createAssCard(assignment);
+        container.appendChild(card);
+    }
+}
+renderAss(assignments);
