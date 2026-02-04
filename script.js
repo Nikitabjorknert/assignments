72fa74ae120ff7e4ec267a01cf21e07055b4071f
Nikita Björknert <nb222tx@student.lnu.se>
2026-02-04 19:59:56 +0100

    stilsättning

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 const assignments = [
     {
-        id: '1a',
-        title: 'Hello',
+        id: '1',
+        title: 'Samlingssida för inlämningsuppgifter',
         description: 'Lära sig',
-        week: 1,
         completed: true,
         showLink: true,
-        difficulty: 'easy',
-        topics: ['Metoder', 'export', 'import']
     },
     {
-        id: '1b',
+        id: '2',
         title: 'Hello2',
         description: 'Lära sig att',
-        week: 1,
         completed: false,
-        showLink: false,
-        difficulty: 'hard',
-        topics: ['git', 'versionshantering']
+        showLink: true,
     }
 ];
 console.log("Lista");
@@ -29,10 +23,8 @@ function createCard(ass) {
     div.innerHTML = `
     <h3>Uppgift ${ass.id} - ${ass.title}</h3>
     <p>${ass.description}</p>
-    <p>${ass.topics}</p>
     <div class="ass-meta">
         <span class="status ${ass.completed ? 'completed' : ''}"></span>
-        <span class="week">Vecka ${ass.week}</span>
     </div>
     ${ass.showLink ? `<a href="./${ass.id}/" class="ass-link">Öppna uppgift</a> 
     ` : ''}
