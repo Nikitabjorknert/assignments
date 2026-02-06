8376b47c723049a6d402d506e244a9e5beb3feb1
Nikita Björknert <nb222tx@student.lnu.se>
2026-02-06 10:02:14 +0100

    Länkarna

--- assignments.js
+++ assignments.js
@@ -5,13 +5,13 @@ export const assignments = [
         id: 'Uppgift 1',
         title: 'Samlingssida för inlämningsuppgifter',
         description: 'Uppgift 1 handlar om att skapa en dynamisk landningssida för alla uppgifter. Varje sida/uppgift är stilsatta på olika sätt för att lättare skilja mellan dem. Sidan är byggd med JavaScript, css och HTML.',
-        link: "../assignment1/index.html",
+        link: "assignment1/index.html",
     },
     {
         id: 'Uppgift 2',
         title: 'Produktsida med kundvagn',
         description: 'Kort beskrivning',
-        link: "../assignment2/index.html",
+        link: "assignment2/index.html",
     }
 ];
 console.log("Lista");
