6f3f33320654451dcbcf824b3402e91905dde727
Nikita Björknert <nb222tx@student.lnu.se>
2026-02-06 17:16:52 +0100

    U2 början

---
+++ assignment2/products.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+        id: 1,
+        name: 'Soffa',
+        description: 'Denna soffa...',
+        price: 14900,
+        image: 'länk',
+        category: ['Soffor', 'Sittmöbler', 'Vardagsrum']
+    },
+    {
+        id: 2,
+        name: 'Matbord',
+        description: 'Denna är...',
+        price: 2500,
+        image: 'länk',
+        category: ['Kitchen', 'Tables', 'Diningroom']
+    },
+    {
+        möbel: 'Lampor'
+    },
+    {
+        möbel: 'Fåtöljer'
+    },
+    {
+        möbel: 'Garderober'
+    },
+    {
+        möbel: 'TV-bänkar'
+    },
+    {
+        möbel: 'Matstolar'
+    },
+    {
+        möbel: 'Kontorsstolar'
+    }
+];
