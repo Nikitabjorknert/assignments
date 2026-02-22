8dbe1ff3b7d7104706691ca59ab63a7be3c128db
Nikita Björknert <nb222tx@student.lnu.se>
2026-02-22 19:18:20 +0100

    U3 layout

--- assignment3/main.js
+++ assignment3/main.js
@@ -4,3 +4,21 @@ fetch("contestants.json")
         console.log(contestants);
         console.log(contestants[0].id);
     });
+
+import { match1 } from "./match.js";
+import { assignments } from "../assignments.js";
+import { renderNav } from "../nav.js";
+
+const rubrik = document.getElementById('rubrik');
+const assignment = assignments.find(assignments => assignments.id === 'Uppgift 3');
+const h3 = document.createElement('h3');
+const h1 = document.createElement('h1');
+const a = document.createElement('a');
+
+h3.textContent = assignment.id;
+h1.textContent = assignment.title;
+a.href = "../index.html";
+a.textContent = 'Tillbaka till start';
+
+rubrik.append(h1, h3, a);
+renderNav(1);
--- assignment3/match.js
+++ assignment3/match.js
@@ -9,12 +9,78 @@
 
 //Toggle-knapp? på den klass med vinnare
 
-/*- class Match {
+export class Match {
 #player1;
 #player2;
 #winner;
-constructor(player1, player2) {
-this.#player1 = player1;
-this.#player2 = player2;
+    constructor(player1, player2) {
+        this.#player1 = player1;
+        this.#player2 = player2;
+
+        this.#winner = "";
+        this.container = ""; //Hela diven som matchen renderas i, så att den kan uppdateras när matchen spelas
+        this.played = false;
+    } 
+
+get players() {
+    return `${this.#player1} vs ${this.#player2}`;
+}
+get winner() {
+    return `${this.#winner}`;
+}
+get isPlayed() {
+return this.played ? 'Game over' : 'Inte spelad än';
+} 
+
+renderMatch() {
+const div = document.createElement('div');
+div.classList.add('match');
+
+const header = document.createElement('div');
+header.classList.add('header');
+header.textContent = this.players;
+
+const status = document.createElement('div');
+status.classList.add('status');
+status.textContent = this.isPlayed;
+
+const btn = document.createElement('button');
+btn.classList.add('play-btn');
+btn.textContent = 'Börja spela';
+btn.addEventListener('click', () => {
+this.play();
+status.textContent = this.isPlayed; //Uppdaterar status på matchen när knappen klickas
+});
+
+div.append(header, status, btn); //Lägger till elementen i diven
+
+this.container = div;
+return div;
+}
+
+play() { //Kolla och gör om!!!
+if (this.played) return;
+this.#winner = Math.random() < 0.5 ? `${this.#player1} vinner!` : `${this.#player2} vinner!`;
+this.played = true;
 }
 }
+
+export const match1 = new Match('Alice', 'Bob'); //Ska man skriva ut deras namn eller ska det bli random?
+document.querySelector('main').append(match1.renderMatch());
+
+
+
+/*--
+
+set winner(score) {
+if (score === 1) {
+this.#winner = this.#player1;
+return;
+} else if (score === 2) {
+this.#winner = this.#player2;
+return;
+}
+}
+
+
+-*/
