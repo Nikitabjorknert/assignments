906c92b5e429782d2e3295f08b5634eaa40ee692
Nikita Björknert <nb222tx@student.lnu.se>
2026-03-12 10:42:20 +0100

    Exempel på layout Assignment 4

--- assignment4/js/index.js
+++ assignment4/js/index.js
@@ -1,8 +1,23 @@
-fetch("data/house.json")
-  .then(response => response.json())
-  .then(data => {
+
+async function fetchData() {
+    const response = await fetch('data/house.json');
+    let data = await response.json();
     console.log(data);
-  });
+}
+fetchData();
 
 
-  
+function renderCards() {
+    const cards = document.createElement('div');
+    cards.classList.add('houseCards');
+    console.log(data);
+
+    cards.innerHTML = `
+    <img src="${data.image}" alt="${data.name}">
+    <h2>${data.name}</h2>
+    <h4>${data.location}</h4>
+    <p>${data.coordinates}</p>
+    <p>${data.description}</p>
+    `;
+    return cards;
+}
